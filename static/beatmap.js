342802368d42b7abe995dc3344406908f69c5390
cmyui <cmyuisuki@gmail.com>
2018-02-13 12:07:02 +0000

    Beatmap fix, and maybe other things?

--- static/beatmap.js
+++ static/beatmap.js
@@ -8,7 +8,7 @@
     var wl = window.location;
     window.history.replaceState('', document.title,
       "/b/" + b + "?mode=" + m + wl.hash);
-    api("scores", {
+    api("scores?sort=score,desc&sort=id,asc", {
       mode : m,
       b : b,
       p : 1,
@@ -22,19 +22,25 @@
         data.scores = [];
       }
       var i = 0;
+      data.scores.sort(function(a, b) { return b.score - a.score; });
       data.scores.forEach(function(score) {
         var user = score.user;
         tb.append($("<tr />").append(
-          $("<td />").text("#" + ((page - 1) * 50 + (++i))),
+          $("<td data-sort-value=" + (++i) + " />")
+            .text("#" + ((page - 1) * 50 + i)),
           $("<td />").html("<a href='/u/" + user.id +
                                  "' title='View profile'><i class='" +
                                  user.country.toLowerCase() + " flag'></i>" +
                                  escapeHTML(user.username) + "</a>"),
-          $("<td />").html(addCommas(score.score)),
-          $("<td />").html(modbits.string(score.mods)),
-          $("<td />").text(score.accuracy.toFixed(2) + "%"),
-          $("<td />").text(addCommas(score.max_combo)),
-          $("<td />").html(score.pp.toFixed(2))));
+          $("<td data-sort-value=" + score.score + " />")
+            .html(addCommas(score.score)),
+          $("<td />").html(getScoreMods(score.mods, true)),
+          $("<td data-sort-value=" + score.accuracy + " />")
+            .text(score.accuracy.toFixed(2) + "%"),
+          $("<td data-sort-value=" + score.max_combo + " />")
+            .text(addCommas(score.max_combo)),
+          $("<td data-sort-value=" + score.pp + " />")
+            .html(score.pp.toFixed(2))));
       });
     });
   }
@@ -56,6 +62,20 @@
     $("#drainLength").html(timeFormat(diff.HitLength));
     $("#bpm").html(diff.BPM);
 
+    // hide mode for non-std maps
+    console.log("favMode", favMode);
+    if (diff.Mode != 0) {
+      currentMode = (currentModeChanged ? currentMode : favMode);
+      $("#mode-menu").hide();
+    } else {
+      currentMode = diff.Mode;
+      $("#mode-menu").show();
+    }
+
+    // update mode menu
+    $("#mode-menu .active.item").removeClass("active");
+    $("#mode-" + currentMode).addClass("active");
+
     loadLeaderboard(bid, currentMode);
   }
   window.loadLeaderboard = loadLeaderboard;
@@ -76,71 +96,7 @@
       $(this).addClass("active");
       currentMode = $(this).data("mode");
       loadLeaderboard(beatmapID, currentMode);
+      currentModeChanged = true;
     });
+  $("table.sortable").tablesort();
 })();
-var modbits = {
-  nomod : 0,
-  nf : 1 << 0,
-  ez : 1 << 1,
-  ts : 1 << 2,
-  hd : 1 << 3,
-  hr : 1 << 4,
-  dt : 1 << 6,
-  ht : 1 << 8,
-  nc : 1 << 9,
-  fl : 1 << 10,
-  so : 1 << 12,
-};
-modbits.from_string = function(str) {
-  var mask = 0;
-  str = str.toLowerCase();
-  for (var property in modbits) {
-    if (property.length != 2) {
-      continue;
-    }
-    if (!modbits.hasOwnProperty(property)) {
-      continue;
-    }
-    if (str.indexOf(property) >= 0) {
-      mask |= modbits[property];
-    }
-  }
-  return mask;
-};
-modbits.string = function(mods) {
-  var res = Array();
-  for (var property in modbits) {
-    if (property.length != 2) {
-      continue;
-    }
-    if (!modbits.hasOwnProperty(property)) {
-      continue;
-    }
-    if (mods & modbits[property]) {
-      res.push(property.toUpperCase());
-    }
-  }
-  return res.join(",");
-};
-// time format (seconds -> hh:mm:ss notation)
-function timeFormat(t) {
-  var h = Math.floor(t / 3600);
-  t %= 3600;
-  var m = Math.floor(t / 60);
-  var s = t % 60;
-  var c = "";
-  if (h > 0) {
-    c += h + ":";
-    if (m < 10) {
-      c += "0";
-    }
-    c += m + ":";
-  } else {
-    c += m + ":";
-  }
-  if (s < 10) {
-    c += "0";
-  }
-  c += s;
-  return c;
-}
